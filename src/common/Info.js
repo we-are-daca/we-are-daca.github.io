82f54e00eb288bf6ed252bb36fd244c0a6869956
Daniel Diaz <diazjfdaniel@gmail.com>
2017-09-17 16:24:30 -0700

    React icons

--- src/common/Info.js
+++ src/common/Info.js
@@ -1,21 +1,37 @@
 import React from 'react';
+import Calendar from 'react-icons/lib/fa/calendar-o';
+import Flag from 'react-icons/lib/fa/flag-o';
+import ClockO from 'react-icons/lib/fa/clock-o';
+import Suitcase from 'react-icons/lib/fa/suitcase';
 
 const Info = (props) => (
     <div className="col-md-6">
         <li>
-            <p>{'YEARS IN US'}</p>
+            <p>
+                {'YEARS IN US '}
+                <ClockO />
+            </p>
             <p>{props.years}</p>
         </li>
         <li>
-            <p>{'OCCUPATION'}</p>
+            <p>
+                {'OCCUPATION'}
+                <Suitcase />
+            </p>
             <p>{props.occupation}</p>
         </li>
         <li>
-            <p>{'Country of Origin'}</p>
+            <p>
+                {'Country of Origin'}
+                <Flag />
+            </p>
             <p>{props.country}</p>
         </li>
         <li>
-            <p>{'Interview Date'}</p>
+            <p>
+                {'Interview Date'}
+                <Calendar />
+            </p>
             <p>{props.date}</p>
         </li>
     </div>
