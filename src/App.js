3eb440aceb61e5c80845989ce9dee9054234dd8a
Daniel Diaz <diazjfdaniel@gmail.com>
2017-09-16 23:44:55 -0700

    Fix viewport, overflow, media query for font

--- src/App.js
+++ src/App.js
@@ -138,7 +138,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <div className="App" style={{ overflowX: 'hidden' }}>
         <Loading
           color={'black'}
           show={this.state.show}
