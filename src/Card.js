fc80380bfa6f0c8b14820d07ed6fd5ed57aad803
Daniel Diaz <diazjfdaniel@gmail.com>
2017-10-11 13:15:50 -0700

    Fix mobile

--- src/Card.js
+++ src/Card.js
@@ -6,7 +6,7 @@ class Card extends Component {
 
   render() {
     return (
-      <div className="Card-content col-12 col-sm-12 col-md-3" align="center">
+      <div className="Card-content col-6 col-sm-6 col-md-3" align="center">
         <div className="innercardcontent">
         <div className="hovereffect">
           <img className='Card-images' src={`https://dwistynbcri9g.cloudfront.net/${this.props.image}`} alt={this.props.name}
