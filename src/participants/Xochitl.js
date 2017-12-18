9059f9b88e7e876e316710ad01c5c385bb841bea
Daniel Diaz <diazjfdaniel@gmail.com>
2017-12-17 20:11:13 -0800

    Change icon color

--- src/participants/Xochitl.js
+++ src/participants/Xochitl.js
@@ -202,7 +202,7 @@ class Xochitl extends React.Component {
                           size={40}
                           onClick={this.handleMenu}
                           className="menu-icon"
-                          color={"white"}
+                          color={"black"}
                           style={{ margin: 10 }}
                         />
                       )}
